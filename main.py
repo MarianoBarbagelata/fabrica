a1a7c5350a4866938bf72b7de8a8474a4d28306a
MarianoBarbagelata <marianoabarba@gmail.com>
2023-06-01 21:03:58 -0300

    Creada la base de la aplicacion

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# hola lukita
--holaasldm,ald 
+import customtkinter
+from tkinter        import ttk
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
